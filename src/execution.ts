54917a03fd8a9df36913755c2a9aee00a6a90615
Tom Lau <tomandfatboy@gmail.com>
2024-02-03 10:51:32 +0800

    Keep only the most recently spawned lint task for each file

--- src/execution.ts
+++ src/execution.ts
@@ -21,10 +21,23 @@ export interface FailedExecution {
     result?: Result;
 }
 
-export function processString(cmd: string, args: string[], opt: Option, input: string): Thenable<Result> {
+// for any filename, keep only the most recently spawned process
+const procMap = new Map<string, child_process.ChildProcess>();
+
+export function processString(cmd: string, args: string[], opt: Option, input: string, filename: string): Thenable<Result> {
     return new Promise((resolve, reject) => {
         let proc = child_process.execFile(cmd, args, opt,
             (error, stdout, stderr) => {
+                // reject if cancelled
+                if (procMap.get(filename) !== proc) {
+                    reject(<FailedExecution>{
+                        errorCode: ErrorCode.Cancel,
+                        result: <Result>{ error, stdout, stderr }
+                    });
+                    return;
+                }
+                procMap.delete(filename);
+
                 if (error != null && error.message === 'stdout maxBuffer exceeded.') {
                     reject(<FailedExecution>{
                         errorCode: ErrorCode.BufferLimitExceed,
@@ -35,5 +48,12 @@ export function processString(cmd: string, args: string[], opt: Option, input: s
                 }
             });
         proc.stdin!.end(input);
+
+        // kill previous spawned process if exist
+        const prevProc = procMap.get(filename);
+        procMap.set(filename, proc);
+        if (prevProc !== undefined) {
+            prevProc.kill();
+        }
     });
 }
